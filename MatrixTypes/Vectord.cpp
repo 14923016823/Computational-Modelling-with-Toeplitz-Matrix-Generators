6fff332291be6fade7985de48283c1eda7eab108
14923016823 <nziubrys@tudelft.nl>
2025-12-15 09:05:08 +0100

    Source update in correspondace to Vectord.h update

--- MatrixTypes/Vectord.cpp
+++ MatrixTypes/Vectord.cpp
@@ -1,50 +1,141 @@
 #include "Vectord.h"
 
+#include <algorithm>
+#include <cmath>
 
-Vectord::Vectord(int length = 0) 
+Vectord::Vectord()
+    : Length(0), Vec(nullptr)
+{}
+
+Vectord::Vectord(int n)
+    : Length(n), Vec(n > 0 ? new double[n] : nullptr)
 {
-    Length=length;
-    Vec=new double[Length];
-    for(int i=0;i<Length;i++)
-    {
-        Vec[i]=0;
-    }
+    std::fill(Vec, Vec + Length, 0.0);
 }
 
-int Vectord::len()
+Vectord::Vectord(const Vectord& other)
+    : Length(other.Length), Vec(other.Length > 0 ? new double[other.Length] : nullptr)
 {
-    return Length;
+    std::copy(other.Vec, other.Vec + Length, Vec);
+}
+
+Vectord::Vectord(Vectord&& other) noexcept
+    : Length(other.Length), Vec(other.Vec)
+{
+    other.Length = 0;
+    other.Vec = nullptr;
+}
+
+Vectord& Vectord::operator=(const Vectord& other)
+{
+    if (this == &other) return *this;
+
+    resize(other.Length);
+    std::copy(other.Vec, other.Vec + Length, Vec);
+    return *this;
+}
+
+Vectord& Vectord::operator=(Vectord&& other) noexcept
+{
+    if (this == &other) return *this;
+
+    delete[] Vec;
+    Length = other.Length;
+    Vec = other.Vec;
+
+    other.Length = 0;
+    other.Vec = nullptr;
+    return *this;
 }
 
 Vectord::~Vectord()
 {
-    delete Vec;
+    delete[] Vec; 
 }
 
-/*~Vectord()
+int Vectord::len() const
 {
-    Delete
-}*/
+    return Length;
+}
 
-void Vectord::PrintVector()
+double& Vectord::operator[](int i)
+{
+    return Vec[i];
+}
+
+const double& Vectord::operator[](int i) const
+{
+
+    return Vec[i];
+}
+
+double* Vectord::data()
+{
+    return Vec;
+}
+
+const double* Vectord::data() const
+{
+    return Vec;
+}
+
+void Vectord::resize(int n)
+{
+    if (n == Length) return;
+
+    delete[] Vec;
+    Length = n;
+    Vec = (n > 0) ? new double[n] : nullptr;
+    std::fill(Vec, Vec + Length, 0.0);
+}
+
+void Vectord::fill(double value)
+{
+    std::fill(Vec, Vec + Length, value);
+}
+
+double Vectord::dot(const Vectord& other) const
+{
+    if (Length != other.Length)
+        throw std::runtime_error("Vectord::dot size mismatch");
+
+    double s = 0.0;
+    for (int i = 0; i < Length; ++i)
+        s += Vec[i] * other.Vec[i];
+
+    return s;
+}
+
+double Vectord::norm2() const
+{
+    return this->dot(*this);
+}
+
+double Vectord::norm() const
+{
+    return std::sqrt(norm2());
+}
+
+void Vectord::scal(double a)
 {
     for (int i = 0; i < Length; ++i)
-    {
-        printf("vec[%d]=%f\n", i, Vec[i]);
-    }
-    printf("done\n");
+        Vec[i] *= a;
 }
 
-void Vectord::Sum(const Vectord& VecIn)
+void Vectord::axpy(double a, const Vectord& x)
 {
-    if (VecIn.Length != Length)
-    {
-        throw std::invalid_argument("You can't sum vectors with different sizes");
-    }
-    for (int i = 0; i < Length; i++)
-    {
-        Vec[i] += VecIn.Vec[i];
-    }
+    if (Length != x.Length)
+        throw std::runtime_error("Vectord::axpy size mismatch");
+
+    for (int i = 0; i < Length; ++i)
+        Vec[i] += a * x.Vec[i];
 }
 
+void Vectord::axpby(double a, double b, const Vectord& x)
+{
+    if (Length != x.Length)
+        throw std::runtime_error("Vectord::axpby size mismatch");
 
+    for (int i = 0; i < Length; ++i)
+        Vec[i] = a * Vec[i] + b * x.Vec[i];
+}
