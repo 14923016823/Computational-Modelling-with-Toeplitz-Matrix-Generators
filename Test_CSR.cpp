b2ff0e222ff29f643bfc6fb62b144ee12b613d2e
Olaf-TUD <o.f.vandam@student.tudelft.nl>
2026-01-19 14:42:56 +0100

    Add files via upload
    Use one test at a time

---
+++ Test_CSR.cpp
@@ -0,0 +1,21 @@
+#include "Test_CSR.h"
+
+int main()
+{
+    int STlength = 3;
+    int STdiags[STlength] = {-1,0,1};
+    double STvals[STlength] = {-1,2,-1};
+    int STwidth = 400;
+    int STheight = 400;
+    Vectord x(STwidth);
+    for(int i=0;i<STwidth;i++)
+    {
+        x.Vec[i]=i;
+    }
+    SparseToeplitz C(STheight, STwidth, STlength, STdiags, STvals);
+
+    CSR C_CSR(C);
+    Vectord a = C_CSR*x;
+
+    return 0;
+}
---
+++ Test_ST.cpp
@@ -0,0 +1,20 @@
+#include "Test_ST.h"
+
+int main()
+{
+    int STlength = 3;
+    int STdiags[STlength] = {-1,0,1};
+    double STvals[STlength] = {-1,2,-1};
+    int STwidth = 400;
+    int STheight = 400;
+    Vectord x(STwidth);
+    for(int i=0;i<STwidth;i++)
+    {
+        x.Vec[i]=i;
+    }
+    SparseToeplitz C(STheight, STwidth, STlength, STdiags, STvals);
+
+    Vectord a = C*x;
+
+    return 0;
+}
