c9e7c989263322decab887061f93f0c7ee4e4eae
Ruben1509 <60895646+Ruben1509@users.noreply.github.com>
2026-01-22 15:24:38 +0100

    Add files via upload

--- Laplacian2d.h
+++ Laplacian2d.h
@@ -4,9 +4,9 @@
 class Laplacian2D_ToeplitzMatrix :Matrix{
     public:
 
-    Laplacian2D_ToeplitzMatrix(const int rows, const int cols, Vectord& b1);
+    Laplacian2D_ToeplitzMatrix(const int rows, const int cols);
 
-    void generateMatrix(const int rows, const int cols, Vectord& b1);
+    void generateMatrix(const int rows, const int cols);
 
     Vectord Laplacian(Vectord& input);
     
@@ -27,27 +27,6 @@ class Laplacian2D_ToeplitzMatrix :Matrix{
     double operator()(int i, int j) const override;
 
     
-
-    //Vectord generateMatrix(const int rows, const int cols, Vectord& b1);
-};
-
-class Laplacian3D_ToeplitzMatrix {
-    public:
-
-    Laplacian3D_ToeplitzMatrix(const int rows, const int cols, const int arrays, Vectord& b1);
-
-    void generateMatrix(const int rows, const int cols, const int arrays, Vectord& b1);
-
-    Vectord Laplacian(Vectord& input);
-    
-    private:
-    Matrix* Incidence; //the geometry of the problem
-    Matrix* Diagonal;//This matrix encapsulates grid spacing and variable k
-    Matrix* Incidence_T;//negative transpose of incidence matrix
-
-
-
-    double k_func(double x, double y, double z);    
 
     //Vectord generateMatrix(const int rows, const int cols, Vectord& b1);
 };
