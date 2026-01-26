eda77bcac6bbcd1abf3a7180d335768cb90b9927
Glenniebrother <42620113+Glenniebrother@users.noreply.github.com>
2026-01-26 10:37:30 +0100

    Changed tests

--- Test_COO_2.cpp
+++ Test_COO_2.cpp
@@ -17,8 +17,8 @@ int main()
         n++;
     }
 
-    int STwidth = 40000;
-    int STheight = 40000;
+    int STwidth = 400000;
+    int STheight = 400000;
     Vectord x(STwidth);
     for(int i=0;i<STwidth;i++)
     {
