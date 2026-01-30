2f11381a853e50189b9640e10a199557fa3bd544
Olaf-TUD <o.f.vandam@student.tudelft.nl>
2026-01-30 22:44:20 +0100

    Tests for Toeplitz Laplacian

---
+++ Test_L2_T.cpp
@@ -0,0 +1,20 @@
+#include "Test_L2_T.h"
+
+int main()
+{  
+    //Change the dimensions as needed
+    int rows = 10000;
+    int cols = 10000;
+
+    Vectord vec2(cols*rows);
+    for (int i = 0; i < rows * cols; ++i) {
+        vec2.Vec[i] = i; // Example initialization
+    }
+    return 0;
+
+    Laplacian2D_ToeplitzMatrix L2d=Laplacian2D_ToeplitzMatrix(rows,cols);
+    Vectord a(rows*cols);
+    //Change the amount of loops as needed
+    for(int i=0;i<10;i++)
+        L2d.Laplacian2d(vec2,a);
+}
---
+++ Test_L2_T.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "COO.h"
+#include "CSR.h"
+#include "DiagonalMatrix.h"
+#include "BlockToeplitz.h"
+#include "DiagonalMatrix.h"
+#include "Laplacian2d.h"
