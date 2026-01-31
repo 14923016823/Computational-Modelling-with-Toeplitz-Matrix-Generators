d162a9e14a8f7d372d772e98d58b2d46cd1ba99e
Olaf-TUD <o.f.vandam@student.tudelft.nl>
2026-01-31 09:36:03 +0100

    Update Test_L3_COO.cpp

--- Test_L3_COO.cpp
+++ Test_L3_COO.cpp
@@ -13,11 +13,12 @@ int main()
     }
 
     Laplacian3D_ToeplitzMatrix L3d=Laplacian3D_ToeplitzMatrix(rows,cols,arrays);
-    Vectord a(rows*cols);
+    Vectord a(rows*cols*arrays);
     COO L3d_COO = L3d.COO_Laplacian();
     //Change the amount of loops as needed
     for(int i=0;i<10;i++)
         L3d_COO.MatMul(vec3,a);
 
     return 0;
+
 }
