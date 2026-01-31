7068705eb937470618fb2cefe484f920a4ffc055
Glenniebrother <42620113+Glenniebrother@users.noreply.github.com>
2026-01-31 13:00:51 +0100

    This project is going to be the end of me istg

--- Test_L3_T.cpp
+++ Test_L3_T.cpp
@@ -17,7 +17,6 @@ int main()
     Vectord a(rows*cols*arrays);
     //Change the amount of loops as needed
     for(int i=0;i<100;i++)
-        std::cout << "Iteration " << i << std::endl;
         L3d.Laplacian3d(vec3,a);
 
     return 0;
