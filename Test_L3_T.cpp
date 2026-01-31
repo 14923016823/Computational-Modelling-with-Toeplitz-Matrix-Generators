57df64aca69e83373a05c3f331bf266ae4d1706a
Glenniebrother <42620113+Glenniebrother@users.noreply.github.com>
2026-01-31 13:03:06 +0100

    See previous push

--- Test_L3_T.cpp
+++ Test_L3_T.cpp
@@ -16,7 +16,7 @@ int main()
     std::cout << "Laplacian generated" << std::endl;
     Vectord a(rows*cols*arrays);
     //Change the amount of loops as needed
-    for(int i=0;i<100;i++)
+    for(int i=0;i<10;i++)
         L3d.Laplacian3d(vec3,a);
 
     return 0;
